9c833cb25558ce2a1d4888233cb77ebc36cf6a36
Volodymyr <nebesnyivr@gmail.com>
2023-03-29 19:20:39 +0300

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import { But } from './Button.styled';
 export const Button = ({ onClick }) => {
   return (
     <But type="button" onClick={onClick}>
-      Load more
+      Load More
     </But>
   );
 };
